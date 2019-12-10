a1210f539bdc16a4f5b79ad7fffca84110235147
Omidiora Emmanuel <45630269+omidiora@users.noreply.github.com>
2019-12-10 21:50:23 +0100

    index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,160 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/app.css">
+  <title id="font">SIMPLY STUDENT FAIR</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,800&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
+
+<!-- include summernote css/js -->
+<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
+<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom fonts for this template -->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- Custom styles for this template -->
+<link href="css/landing-page.min.css" rel="stylesheet">
+
 </head>
-<body>
-        <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation"></button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a class="dropdown-item" href="#">Action 1</a>
-                                <a class="dropdown-item" href="#">Action 2</a>
-                            </div>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
-        
-        <script src="/js/app.js"></script>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse w-100 order-1 order-lg-0" id="navbarNav">
+            <ul class="navbar-nav ml-auto ">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item active mr-4">
+                        <a class="nav-link" href="">About us</a>
+                    </li>
+
+                    <li class="nav-item active mr-4">
+                            <a class="nav-link" href=""></a>
+                      </li>
+                
+            </ul>
+        </div>
+        <div class="d-flex w-100 order-0">
+            <div class="w-100">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+           
+            <span class="w-100"></span>
+        </div>
+     
+    </nav>
     
+    
+    
+
+
+<body>
+
+<!-- Masthead -->
+
+
+
+<header class="masthead text-white text-center">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-9 mx-auto">
+              <h1 class="mb-5">TITLE</h1>
+            </div>
+            <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                    CONNECTING AFRICAN STARTUPS TO OPPORTUNITIES
+                    VC4A is connecting startup entrepreneurs with the knowledge, support programs, mentors and investors they require to succeed.
+            </div>
+          </div>
+        </div>
+      </header>
+    
+  
+  
+  <!---------->
+  <div class="container mt-4"  style="margin-top:180px!important">
+        <div class="mt-4">
+     <div class="card" style="background:gray">
+            <h4 class="d-flex justify-content-center"  style="color:white"> Sign Up for business details registration</h4>
+           
+            <div class="form-group">
+                <label for=""   style="color:white;font-size:25px" class="ml-4" >Email</label>
+                <input type="email" class="form-control">
+            </div>
+
+            <div class="form-group">
+                    <label for=""  style="color:white;font-size:25px" class="ml-4">Password</label>
+                    <input type="password" class="form-control">
+                </div>
+                <div class="form-group">
+                        <label for=""  style="color:white;font-size:25px" class="ml-4">retype-Password</label>
+                        <input type="password" class="form-control">
+                    </div>
+            <div class="form-group">
+                   <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+
+            </div>
+     </div>
+  </div>
+  
+
+        </div>
+        
+
+  
+
+
+
+    
+<footer style="margin-top:140px; background:black ;padding:50px;color:white">
+<div class="container-fluid mt-4">
+<div class="row">
+<div class="col-md-4">
+
+</div>
+<div class="col-md-4jj">
+    <h6><a href="/" class="nav-link">HOME </a> </h6>
+<h6> <a href="" class="nav-link">ABOUT </a></h6>
+   
+        </div>
+        <div class="col-md-4">
+                <p>Lorem ipsum quam deserunt sed iusto ipsa assumenda, temporibus repellendus quia odio magni iste. Tempore, nisi illum?</p>
+                </div>
+    
+        
+</div>
+
+</div>
+    
+
+</footer>
+<script>
+
+$('.carousel').carousel({
+    interval: 100
+  });
+</script>
+<script src="js/app.js"></script>
+<script src="js/bootstrap.js"></script>
+
 </body>
 </html>
+
+
+
+
+
+
+
+
